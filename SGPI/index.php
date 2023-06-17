5da1bc97c2c6efe72e0dcdc6de55bad5fc9360cb
dipoljoseph <97227464+dipoljoseph@users.noreply.github.com>
2023-06-17 17:43:37 +0800

    Add files via upload

---
+++ SGPI/index.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="style2.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <title>Sapphire Gate</title>
+</head>
+<body>
+    <!--header-->
+    <div class="header column">
+        <div class="left-head">
+            <img src="sapphire philipphines\pin.png" class="header-img">
+            <label>Unit 2404, Discovery Suites, ADB Ave. Ortigas Center, Pasig City, Philippines</label>
+        </div>
+
+        <div class="sign-in right-head">
+            <img src="sapphire philipphines\sign in icon.png" class="header-img">
+            <label>Sign In</label>
+        </div>
+    </div>
+
+    <!--navigation-->
+    <nav>
+        <div class="nav column">
+            <div class="left-logo left">
+                <img src="sapphire philipphines\Logo(nav).png" class="logo-navbar">
+            </div>
+
+            <div class="row right">
+                <div class="right-top">
+                    <img src="sapphire philipphines\001-facebook-circular-logo.png" class="nav-socmed">
+                    <img src="sapphire philipphines\002-instagram.png" class="nav-socmed">
+                    <img src="sapphire philipphines\yt.png" class="nav-socmed">
+                    <img src="sapphire philipphines\004-linkedin.png" class="nav-socmed">
+                </div>
+
+                <div class="right-bot">
+                    <input type="checkbox" id="check">
+                    <label for="check" class="checkbtn">
+                        <i class="fas fa-bars"></i>
+                    </label>
+                    <ul> 
+                        <li><a href="#" class="active">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">All Properties</a></li>
+                        <li><a href="#">FAQ's</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!--slider-->
+    <div class="container">
+        <img src="sapphire philipphines/banner image plain.png" class="banner-img">
+        <div class="centered">
+            <h1>Links Your Realty Needs</h1>
+            <br>
+            <p class="card-text">The next leading real estate service provider</p>
+
+            <div class="dropdown">
+                <select name="first" id="land1">
+                    <option value="volvo">For Lease</option>
+                </select>
+                <select name="second" id="land2">
+                    <option value="volvo">Type of Properties</option>
+                </select>
+                <select name="third" id="land3">
+                    <option value="volvo">Search of Location</option>
+                </select>
+            </div>
+
+            <button>OUR SERVICES</button>
+            <button>LIST YOUR PROPERTIES</button>
+            <button>RESEARCH & INSIGHTS</button>
+        </div>
+    </div>
+
+    <!--partner developers-->
+    <div class="part-dev">
+        <h2>Select from our</h2>
+        <h1>Partner Developers</h1>
+
+        <div>
+            <img src="sapphire philipphines/avida-logo-png.png" class="pd-imgs">
+            <img src="sapphire philipphines/2560px-Ayala_Land_logo.svg.png" class="pd-imgs">
+            <img src="sapphire philipphines/REGUS.png" class="pd-imgs">
+            <img src="sapphire philipphines/DATEMHOMES.png" class="pd-imgs">
+            <img src="sapphire philipphines/antel-grand-village-logo.png" class="pd-imgs">
+        </div>
+
+        <div>
+            <img src="sapphire philipphines/CATHAY LAND.png" class="pd-imgs">
+            <img src="sapphire philipphines/CEO SUITE.png" class="pd-imgs">
+            <img src="sapphire philipphines/download (2).png" class="pd-imgs">
+            <img src="sapphire philipphines/dataland-logo.png" class="pd-imgs">
+            <img src="sapphire philipphines/phirst park logo-2.png" class="pd-imgs">
+        </div>
+    </div>
+
+    <!--our services-->
+    <div class="services">
+        <h1>OUR SERVICES</h1>
+        <p class="serv-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+
+        <div class="serv-text column">
+            <div class="left">
+                <h1><span class="dot"></span>Lorem ipsum dolor</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute. irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+            <div class="right">
+                <h1><span class="dot"></span>Lorem ipsum dolor</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute. irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+
+        <div class="serv-text column">
+            <div class="left">
+                <h1><span class="dot"></span>Lorem ipsum dolor</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute. irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+            <div class="right">
+                <h1><span class="dot"></span>Lorem ipsum dolor</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute. irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+
+        <button class="serv-btn">SEE ALL SERVICES</button>
+    </div>
+    
+    <!--featured properties-->
+    <div class="ft-prop">
+        <div class="column">
+            <div class="left">
+                <h1>FEATURED PROPERTIES</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,</p>
+            </div>
+
+            <div class="right">
+                <img src="sapphire philipphines/sample video.png">
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card" style="margin-left: 0">
+                <img src="sapphire philipphines\001.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>Sample Property name</b></h4>
+                    <p>Pellentesque vehicula fermentum eu cursus. Cras convallis tellus et elit aliquet</p>
+                    <button class="see-det">See Details
+                        <img src="sapphire philipphines\arrow.png" alt="Avatar" style="width: 12%">
+                    </button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="sapphire philipphines\002.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>Sample Property name</b></h4>
+                    <p>Pellentesque vehicula fermentum eu cursus. Cras convallis tellus et elit aliquet</p>
+                    <button class="see-det">See Details
+                        <img src="sapphire philipphines\arrow.png" alt="Avatar" style="width: 12%">
+                    </button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="sapphire philipphines\003.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>Sample Property name</b></h4>
+                    <p>Pellentesque vehicula fermentum eu cursus. Cras convallis tellus et elit aliquet</p>
+                    <button class="see-det">See Details
+                        <img src="sapphire philipphines\arrow.png" alt="Avatar" style="width: 12%">
+                    </button>
+                </div>
+            </div>
+
+            <div class="card" style="margin-right: 0">
+                <img src="sapphire philipphines\004.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>Sample Property name</b></h4>
+                    <p>Pellentesque vehicula fermentum eu cursus. Cras convallis tellus et elit aliquet</p>
+                    <button class="see-det">See Details
+                        <img src="sapphire philipphines\arrow.png" alt="Avatar" style="width: 12%">
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="ftprop-btn">
+            <button class="list-btn">LIST YOUR PROPERTY</button>
+            <button class="viewprop-btn">VIEW MORE PROPERTIES</button>
+        </div>
+    </div>
+
+    <!--new listings-->
+    <div class="new-list">
+        <div class="column">
+            <div class="left">
+                <h1>NEW LISTINGS</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card" style="margin-left: 0">
+                <img src="sapphire philipphines\008.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>Sample Property name</b></h4>
+                    <p>Pellentesque vehicula fermentum eu cursus. Cras convallis tellus et elit aliquet</p>
+                    <button class="see-det">See Details
+                        <img src="sapphire philipphines\arrow.png" alt="Avatar" style="width: 12%">
+                    </button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="sapphire philipphines\006.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>Sample Property name</b></h4>
+                    <p>Pellentesque vehicula fermentum eu cursus. Cras convallis tellus et elit aliquet</p>
+                    <button class="see-det">See Details
+                        <img src="sapphire philipphines\arrow.png" alt="Avatar" style="width: 12%">
+                    </button>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="sapphire philipphines\007.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>Sample Property name</b></h4>
+                    <p>Pellentesque vehicula fermentum eu cursus. Cras convallis tellus et elit aliquet</p>
+                    <button class="see-det">See Details
+                        <img src="sapphire philipphines\arrow.png" alt="Avatar" style="width: 12%">
+                    </button>
+                </div>
+            </div>
+
+            <div class="card" style="margin-right: 0">
+                <img src="sapphire philipphines\008.png" alt="Avatar" style="width: 100%">
+                <div class="container">
+                    <h4><b>Sample Property name</b></h4>
+                    <p>Pellentesque vehicula fermentum eu cursus. Cras convallis tellus et elit aliquet</p>
+                    <button class="see-det">See Details
+                        <img src="sapphire philipphines\arrow.png" alt="Avatar" style="width: 12%">
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--list your properties-->
+    <div class="list-prop">
+        <div class="container">
+            <img src="sapphire philipphines/List Properties plain BG.png" class="banner-img">
+            <div class="centered">
+                <h1>LIST YOUR PROPERTY WITH US</h1>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaeca.</p>
+
+                <button>LIST YOUR PROPERTY</button>
+            </div>
+        </div>
+    </div>
+
+    <!--our recognitions-->
+    <div class="our-rec">
+        <h1>OUR RECOGNITIONS</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+
+        <div class="column">
+            <div class="left">
+                <p>“ Vestibulum sollicitudin nunc in eros a justo facilisis rutrum. Aenean id ullamcorper libero. Vestibulum imperdiet nibh tortor mauris condimentum nibh ”</p>
+
+                <div class="column client">
+                    <div class="left-clients">
+                        <img src="sapphire philipphines/Testi 01.png">
+                    </div>
+                    <div class="right">
+                        <span>CLIENT NAME HERE</span>
+                        <span class="span-ceo">CEO lacinia faucibus</span>
+                    </div>
+                </div>
+            </div>
+            <div class="mid">
+                <p>“ Vestibulum sollicitudin nunc in eros a justo facilisis rutrum. Aenean id ullamcorper libero. Vestibulum imperdiet nibh tortor mauris condimentum nibh ”</p>
+
+                <div class="column client">
+                    <div class="left-clients">
+                        <img src="sapphire philipphines/Testi 02.png">
+                    </div>
+                    <div class="right">
+                        <span>CLIENT NAME HERE</span>
+                        <span class="span-ceo">CEO lacinia faucibus</span>
+                    </div>
+                </div>
+            </div>
+            <div class="right last-right">
+                <p>“ Vestibulum sollicitudin nunc in eros a justo facilisis rutrum. Aenean id ullamcorper libero. Vestibulum imperdiet nibh tortor mauris condimentum nibh ”</p>
+
+                <div class="column client">
+                    <div class="left-clients">
+                        <img src="sapphire philipphines/Testi 03.png">
+                    </div>
+                    <div class="right">
+                        <span>CLIENT NAME HERE</span>
+                        <span class="span-ceo">CEO lacinia faucibus</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--footer-->
+    <div class="footer">
+        <div class="column input-fill">
+            <div class="left-input">
+                <h1>WANT TO BE OUR SELLER PARTNER?</h1>
+
+                <input type="text" value="Upload your CV">
+                <input type="text" value="Name">
+                <div class="column">
+                    <input type="text" value="Phone Number" class="left input-foot">
+                    <input type="text" value="Email" class="right input-foot">
+                </div>
+
+                <button>JOIN US!</button>
+            </div>
+
+            <div class="right-input">
+                <h1>BE PART OF OUR TEAM</h1>
+
+                <input type="text" value="Upload your CV">
+                <input type="text" value="Name">
+                <div class="column">
+                    <input type="text" value="Phone Number" class="left input-foot">
+                    <input type="text" value="Email" class="right input-foot">
+                </div>
+
+                <button>SEND</button>
+            </div>
+        </div>
+
+        <div class="column foot-dets">
+            <div class="left-desc">
+                <img src="sapphire philipphines/Logo_white.png" class="footer-logo">
+            </div>
+            <div class="mid-desc">
+                <img src="sapphire philipphines/Footer_pin.png">
+                <span>Unit 2404, Discovery Suites, ADB Ave. Ortigas Center, Pasig City, Philippines</span>
+            </div>
+            <div class="right-desc">
+                <div class="row">
+                    <div class="row-top">
+                        <img src="sapphire philipphines/Footer_FB.png" class="foot-socmed">
+                        <img src="sapphire philipphines/Footer_IG.png" class="foot-socmed">
+                        <img src="sapphire philipphines/Footer_Linkedin.png">
+                        <img src="sapphire philipphines/Footer_mail.png" class="foot-socmed">
+                    </div>
+                    <div class="row-bot">
+                        <p><b>Office Hours</b>
+                            <span>MON - SAT 8:00 AM - 5:00 PM</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <p class="copyright">© 2021 www.sgpi.ph. All rights reserved. &nbsp&nbsp&nbsp Site Map | Terms of Use | Privacy Policy
+            <img src="sapphire philipphines/itworks_logo.png" class="itworks-logo">
+        </p>
+    </div>
+    
+
+</body>
+</html>
